b16bc1da10e0a53478d0e7b89bf73d92008c9552
Ilya Liakh <i.liakh@sdgroup.ai>
2025-06-16 22:31:58 +0200

    Initial Commit

---
+++ githubRepo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubRepo
+//
+//  Created by Ilya Liakh on 16.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
