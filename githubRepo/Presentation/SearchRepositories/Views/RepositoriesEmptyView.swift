fbc6b31490fb790f5ee89eeb243f61ddb0565765
Ilya Liakh <i.liakh@sdgroup.ai>
2025-07-10 10:58:00 +0200

    add entry view

--- githubRepo/Presentation/SearchRepositories/Views/RepositoriesEmptyView.swift
+++ githubRepo/Presentation/SearchRepositories/Views/RepositoriesEmptyView.swift
@@ -5,4 +5,45 @@
 //  Created by Ilya Liakh on 17.06.25.
 //
 
-import Foundation
+import UIKit
+
+public class RepositoriesEmptyView: UIView {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.text = "Sorry, no repositories found, try another search"
+        return label
+    }()
+    
+    private lazy var onsiteLabeel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.text = "Something went wrong"
+        return label
+    }()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        self.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            .init(item: titleLabel, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 20),
+            .init(item: titleLabel, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1.0, constant: -20),
+            .init(item: titleLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1.0, constant: 20),
+        ])
+    }
+}
